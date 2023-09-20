33d03105228e9c5f77adfcfa0e73a2de6920d17d
junaidhkn <junaidhkniazi@gmail.com>
2023-09-21 04:39:53 +0500

    Pointers

--- js/Class/Class.js
+++ js/Class/Class.js
@@ -15,3 +15,20 @@ const cookieTwo = new Cookie( 'Yellow' )
 cookieTwo.setColor( 'Green' )
 // console.log( cookieOne )
 // console.log( cookieTwo )
+
+
+// Pointer Concept
+
+// not a pointer
+
+let num1 = 10
+let num2 = num1
+num2 = 20
+// console.log( num1 ) // 10 num1 remains 10,because num2 is not a pointer
+
+// pointer
+
+let obj1 = { name: 'John' }
+let obj2 = obj1
+obj2.name = 'Bob'
+// console.log( obj1 ) // { name: 'Bob' } obj1 is a pointer to obj2
