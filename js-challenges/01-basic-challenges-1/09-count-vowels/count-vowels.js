5fdeb7158f16424e1b4d82e0a447f2a74e7d1d36
junaidhkn <junaidhkniazi@gmail.com>
2023-11-03 01:34:40 +0500

    count vowels in input

--- js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,13 @@
-function countVowels() {}
+function countVowels ( str ) {
+   let vowels = ['a', 'e', 'i', 'o', 'u']
+   let count = 0;
+   const input = str.toLowerCase()
+   for ( let i = 0; i < input.length; i++ ) {
+      vowels.map( ( vowel ) => {
+         vowel === input[i] ? count++ : count
+      } )
+   }
+   return count
+}
 
 module.exports = countVowels;
