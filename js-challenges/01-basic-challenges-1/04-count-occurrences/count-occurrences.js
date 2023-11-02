582ec172a10a4b229cee4efcf055d4f7a3e7e72f
junaidhkn <junaidhkniazi@gmail.com>
2023-11-02 09:07:02 +0500

    count occurrence

--- js-challenges/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ js-challenges/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,23 @@
-function countOccurrences() {}
+function countOccurrences ( word, charactor ) {
+   // // First Method
+   let count = 0;
+   for ( i = 0; i < word.length; i++ ) {
+      if ( word[i] === charactor ) {
+         count++;
+      }
+   }
+   return count;
+
+   // // Second Method
+   // let count = 0;
+   // let arr = [];
+   // for ( i = 0; i < word.length; i++ ) {
+   //    arr.push( word[i] )
+   // }
+   // arr.map( ( char ) => {
+   //    char === charactor ? count++ : count
+   // } )
+   // return count
+}
 
 module.exports = countOccurrences;
