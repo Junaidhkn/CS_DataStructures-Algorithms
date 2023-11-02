a32c674010afde5e5bab8e03dcb13116441889dd
junaidhkn <junaidhkniazi@gmail.com>
2023-11-03 01:11:53 +0500

    palindrome test

--- js-challenges/01-basic-challenges-1/08-palindrome/palindrome.js
+++ js-challenges/01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,19 @@
-function isPalindrome() {}
+function isPalindrome ( str ) {
+   let palindrome = Boolean;
+   const strModified = str.toLowerCase().split( " " ).join( '' )
+   const input = removeNonAlphanumeric( strModified )
+   for ( let i = 0; i < input.length; i++ ) {
+      if ( input[input.length - i - 1] === input[i] ) {
+         palindrome = true
+      } else {
+         palindrome = false
+      }
+   }
+   return palindrome
+}
+
+const removeNonAlphanumeric = ( input ) => {
+   return input.split( ',' ).join( '' )
+}
 
 module.exports = isPalindrome;
