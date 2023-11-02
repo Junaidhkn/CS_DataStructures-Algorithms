86ae80281fef223e51843bb7235c309421dc1266
junaidhkn <junaidhkniazi@gmail.com>
2023-11-02 12:50:42 +0500

    reverse-string

--- js-challenges/01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ js-challenges/01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString ( input ) {
+   let arr = []
+   for ( let i = 0; i < input.length; i++ ) {
+      arr.push( input.slice()[i] )
+   }
+   return arr.reverse().join( '' )
+}
+
 
 module.exports = reverseString;
