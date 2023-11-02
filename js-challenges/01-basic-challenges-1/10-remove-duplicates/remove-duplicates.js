eda1fbc730514c2268634d40aa5da0aae1e4a810
junaidhkn <junaidhkniazi@gmail.com>
2023-11-03 01:50:41 +0500

    remove duplicate test

--- js-challenges/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ js-challenges/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates ( arr ) {
+   for ( let i = 0; i < arr.length; i++ ) {
+      let input = arr[i]
+      for ( let j = 0; j < arr.length; j++ ) {
+
+
+      }
+   }
+}
 
 module.exports = removeDuplicates;
