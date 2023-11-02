acd584b4dd0be5edc2044553ffa8a45aa21eed4a
junaidhkn <junaidhkniazi@gmail.com>
2023-11-02 08:34:45 +0500

    calculator

--- js-challenges/01-basic-challenges-1/03-calculator/calculator.js
+++ js-challenges/01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,15 @@
-function calculator() {}
+function calculator ( x, y, operation ) {
+   if ( operation == '+' ) {
+      return x + y;
+   } else if ( operation == '-' ) {
+      return x - y;
+   } else if ( operation == '*' ) {
+      return x * y;
+   } else if ( operation == '/' ) {
+      return x / y;
+   } else {
+      return 'Invalid operation';
+   }
+}
 
 module.exports = calculator;
