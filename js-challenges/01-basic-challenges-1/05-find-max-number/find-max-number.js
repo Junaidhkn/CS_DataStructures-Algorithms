e62f2892c108cfa8562631444ac2dbf6dd694738
junaidhkn <junaidhkniazi@gmail.com>
2023-11-02 09:38:09 +0500

    max

--- js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,15 @@
-function findMaxNumber() {}
+function findMaxNumber ( arr ) {
+   let max = arr[0];
+
+   for ( let i = 1; i < arr.length; i++ ) {
+      if ( arr[i] > max ) {
+         max = arr[i];
+      }
+   }
+
+   return max;
+}
+
+console.log( 'Answer:--', findMaxNumber( [13, 5, 12, 9] ) )
 
 module.exports = findMaxNumber;
