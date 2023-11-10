f67a605edb0dc9cd42ea8ecc2fe7a410b89e904d
junaidhkn <junaidhkniazi@gmail.com>
2023-11-10 23:03:06 +0500

    unshift

--- Notes-And-Challenges/03 - Data Structures Linked Lists/009 EXERCISE-LL-Unshift.js
+++ Notes-And-Challenges/03 - Data Structures Linked Lists/009 EXERCISE-LL-Unshift.js
@@ -1,55 +1,55 @@
 class Node {
-    constructor(value){
+    constructor( value ) {
         this.value = value;
         this.next = null;
     }
 }
- 
+
 class LinkedList {
-    constructor(value) {
-        const newNode = new Node(value);
+    constructor( value ) {
+        const newNode = new Node( value );
         this.head = newNode;
         this.tail = this.head;
         this.length = 1;
     }
 
-    printList() {
+    printList () {
         let temp = this.head;
-        while (temp !== null) {
-            console.log(temp.value);
+        while ( temp !== null ) {
+            console.log( temp.value );
             temp = temp.next;
         }
     }
 
-    getHead() {
-        if (this.head === null) {
-            console.log("Head: null");
+    getHead () {
+        if ( this.head === null ) {
+            console.log( "Head: null" );
         } else {
-            console.log("Head: " + this.head.value);
+            console.log( "Head: " + this.head.value );
         }
     }
 
-    getTail() {
-        if (this.tail === null) {
-            console.log("Tail: null");
+    getTail () {
+        if ( this.tail === null ) {
+            console.log( "Tail: null" );
         } else {
-            console.log("Tail: " + this.tail.value);
+            console.log( "Tail: " + this.tail.value );
         }
     }
 
-    getLength() {
-        console.log("Length: " + this.length);
+    getLength () {
+        console.log( "Length: " + this.length );
     }
 
-    makeEmpty() {
+    makeEmpty () {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
- 
-    push(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
+
+    push ( value ) {
+        const newNode = new Node( value );
+        if ( !this.head ) {
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -59,57 +59,73 @@ class LinkedList {
         this.length++;
         return this;
     }
- 
-    pop() {
-        if (this.length === 0) return undefined;
+
+    pop () {
+        if ( this.length === 0 ) return undefined;
         let temp = this.head;
         let pre = this.head;
-        while (temp.next) {
+        while ( temp.next ) {
             pre = temp;
             temp = temp.next;
         }
         this.tail = pre;
         this.tail.next = null;
         this.length--;
-        if (this.length === 0) {
+        if ( this.length === 0 ) {
             this.head = null;
             this.tail = null;
         }
         return temp;
     }
- 
-	/// WRITE UNSHIFT METHOD HERE ///
-	//                             //
-	//                             //
-	//                             //
-	//                             //
-	/////////////////////////////////
- 
- }
- 
 
-function test() {
-    let myLinkedList = new LinkedList(2);
-    myLinkedList.push(3);
+    /// WRITE UNSHIFT METHOD HERE ///
+    //                             //
+    //                             //
+    //                             //
+    //                             //
+    /////////////////////////////////
+    unshift ( value ) {
+        const newNode = new Node( value );
+        if ( !this.head ) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        let temp = this.head
+        this.head = newNode
+        this.head.next = temp
 
-    console.log("Before unshift():");
-    console.log("-----------------");
+        // newNode.next = this.head;
+        // this.head = newNode;
+
+        this.length++
+        return this
+    }
+
+}
+
+
+function test () {
+    let myLinkedList = new LinkedList( 2 );
+    myLinkedList.push( 3 );
+
+    console.log( "Before unshift():" );
+    console.log( "-----------------" );
     myLinkedList.getHead();
     myLinkedList.getTail();
     myLinkedList.getLength();
 
-    console.log("\nLinked List:");
+    console.log( "\nLinked List:" );
     myLinkedList.printList();
 
-    myLinkedList.unshift(1);
+    myLinkedList.unshift( 1 );
 
-    console.log("\nAfter unshift():");
-    console.log("----------------");
+    console.log( "\nAfter unshift():" );
+    console.log( "----------------" );
     myLinkedList.getHead();
     myLinkedList.getTail();
     myLinkedList.getLength();
 
-    console.log("\nLinked List:");
+    console.log( "\nLinked List:" );
     myLinkedList.printList();
 }
 
@@ -144,4 +160,4 @@ test();
 */
 
 
- 
+
