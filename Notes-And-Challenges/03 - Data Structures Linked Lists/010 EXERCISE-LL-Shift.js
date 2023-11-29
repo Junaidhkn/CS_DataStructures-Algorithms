f3372be1c0c984411cd833b5197df9866ccc2d24
junaidhkn <junaidhkniazi@gmail.com>
2023-11-30 00:08:36 +0500

    shift

--- Notes-And-Challenges/03 - Data Structures Linked Lists/010 EXERCISE-LL-Shift.js
+++ Notes-And-Challenges/03 - Data Structures Linked Lists/010 EXERCISE-LL-Shift.js
@@ -1,55 +1,55 @@
 class Node {
-    constructor(value){
+    constructor( value ) {
         this.value = value;
         this.next = null;
     }
 }
- 
+
 class LinkedList {
-    constructor(value) {
-        const newNode = new Node(value);
+    constructor( value ) {
+        const newNode = new Node( value );
         this.head = newNode;
         this.tail = this.head;
         this.length = 1;
     }
 
-    printList() {
+    printList () {
         let temp = this.head;
-        while (temp !== null) {
-            console.log(temp.value);
+        while ( temp !== null ) {
+            console.log( temp.value );
             temp = temp.next;
         }
     }
 
-    getHead() {
-        if (this.head === null) {
-            console.log("Head: null");
+    getHead () {
+        if ( this.head === null ) {
+            console.log( "Head: null" );
         } else {
-            console.log("Head: " + this.head.value);
+            console.log( "Head: " + this.head.value );
         }
     }
 
-    getTail() {
-        if (this.tail === null) {
-            console.log("Tail: null");
+    getTail () {
+        if ( this.tail === null ) {
+            console.log( "Tail: null" );
         } else {
-            console.log("Tail: " + this.tail.value);
+            console.log( "Tail: " + this.tail.value );
         }
     }
 
-    getLength() {
-        console.log("Length: " + this.length);
+    getLength () {
+        console.log( "Length: " + this.length );
     }
 
-    makeEmpty() {
+    makeEmpty () {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
- 
-    push(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
+
+    push ( value ) {
+        const newNode = new Node( value );
+        if ( !this.head ) {
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -59,28 +59,28 @@ class LinkedList {
         this.length++;
         return this;
     }
- 
-    pop() {
-        if (this.length === 0) return undefined;
+
+    pop () {
+        if ( this.length === 0 ) return undefined;
         let temp = this.head;
         let pre = this.head;
-        while (temp.next) {
+        while ( temp.next ) {
             pre = temp;
             temp = temp.next;
         }
         this.tail = pre;
         this.tail.next = null;
         this.length--;
-        if (this.length === 0) {
+        if ( this.length === 0 ) {
             this.head = null;
             this.tail = null;
         }
         return temp;
     }
- 
-    unshift(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
+
+    unshift ( value ) {
+        const newNode = new Node( value );
+        if ( !this.head ) {
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -90,45 +90,62 @@ class LinkedList {
         this.length++;
         return this;
     }
- 
-	/// WRITE SHIFT METHOD HERE ///
-	//                           //
-	//                           //
-	//                           //
-	//                           //
-	///////////////////////////////
- 
- }
- 
 
- function test() {
-    let myLinkedList = new LinkedList(2)
-    myLinkedList.push(1)
+    shift () {
+        let temp
+        if ( !this.head ) { return undefined }
+
+        temp = this.head
+        this.head = this.head.next
+        temp.next = null
+        this.length--
+
+        if ( this.length === 0 ) {
+            this.tail = null
+        }
+        return this
+    }
+
+    /// WRITE SHIFT METHOD HERE ///
+    //                           //
+    //                           //
+    //                           //
+    //                           //
+    ///////////////////////////////
+
+}
+
+
+function test () {
+    let myLinkedList = new LinkedList( 2 )
+    myLinkedList.push( 1 )
+    myLinkedList.push( 4 )
+    myLinkedList.push( 5 )
 
     // (2) Items in LL - Returns 2 Node
-    if (myLinkedList.length !== 0) {
-        console.log(myLinkedList.shift().value);
+    if ( myLinkedList.length !== 0 ) {
+        console.log( myLinkedList.shift().value );
     } else {
-        console.log("null");
+        console.log( "null" );
     }
 
     // (1) Item in LL - Returns 1 Node
-    if (myLinkedList.length !== 0) {
-        console.log(myLinkedList.shift().value);
+    if ( myLinkedList.length !== 0 ) {
+        console.log( myLinkedList.shift().value );
     } else {
-        console.log("null");
+        console.log( "null" );
     }
 
     // (0) Items in LL - Returns null
-    if (myLinkedList.length !== 0) {
-        console.log(myLinkedList.shift().value);
+    if ( myLinkedList.length !== 0 ) {
+        console.log( myLinkedList.shift().value );
     } else {
-        console.log("null");
-    }    
- }
+        console.log( "null" );
+    }
+}
 
 
- test();
+test();
 
 
 /*
