16accbaa093ce087765a4d5ed1aab2315e13a5f5
junaidhkn <junaidhkniazi@gmail.com>
2023-11-30 02:28:46 +0500

    remove item by index

--- Notes-And-Challenges/03 - Data Structures Linked Lists/014 EXERCISE-LL-Remove.js
+++ Notes-And-Challenges/03 - Data Structures Linked Lists/014 EXERCISE-LL-Remove.js
@@ -1,55 +1,55 @@
 class Node {
-    constructor(value){
+    constructor( value ) {
         this.value = value;
         this.next = null;
     }
 }
- 
+
 class LinkedList {
-    constructor(value) {
-        const newNode = new Node(value);
+    constructor( value ) {
+        const newNode = new Node( value );
         this.head = newNode;
         this.tail = this.head;
         this.length = 1;
     }
 
-    printList() {
+    printList () {
         let temp = this.head;
-        while (temp !== null) {
-            console.log(temp.value);
+        while ( temp !== null ) {
+            console.log( temp.value );
             temp = temp.next;
         }
     }
 
-    getHead() {
-        if (this.head === null) {
-            console.log("Head: null");
+    getHead () {
+        if ( this.head === null ) {
+            console.log( "Head: null" );
         } else {
-            console.log("Head: " + this.head.value);
+            console.log( "Head: " + this.head.value );
         }
     }
 
-    getTail() {
-        if (this.tail === null) {
-            console.log("Tail: null");
+    getTail () {
+        if ( this.tail === null ) {
+            console.log( "Tail: null" );
         } else {
-            console.log("Tail: " + this.tail.value);
+            console.log( "Tail: " + this.tail.value );
         }
     }
 
-    getLength() {
-        console.log("Length: " + this.length);
+    getLength () {
+        console.log( "Length: " + this.length );
     }
 
-    makeEmpty() {
+    makeEmpty () {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
- 
-    push(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
+
+    push ( value ) {
+        const newNode = new Node( value );
+        if ( !this.head ) {
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -59,28 +59,28 @@ class LinkedList {
         this.length++;
         return this;
     }
- 
-    pop() {
-        if (this.length === 0) return undefined;
+
+    pop () {
+        if ( this.length === 0 ) return undefined;
         let temp = this.head;
         let pre = this.head;
-        while (temp.next) {
+        while ( temp.next ) {
             pre = temp;
             temp = temp.next;
         }
         this.tail = pre;
         this.tail.next = null;
         this.length--;
-        if (this.length === 0) {
+        if ( this.length === 0 ) {
             this.head = null;
             this.tail = null;
         }
         return temp;
     }
- 
-    unshift(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
+
+    unshift ( value ) {
+        const newNode = new Node( value );
+        if ( !this.head ) {
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -90,83 +90,101 @@ class LinkedList {
         this.length++;
         return this;
     }
- 
-    shift() {
-        if (this.length === 0) return undefined;
+
+    shift () {
+        if ( this.length === 0 ) return undefined;
         let temp = this.head;
         this.head = this.head.next;
         this.length--;
-        if (this.length === 0) {
+        if ( this.length === 0 ) {
             this.tail = null;
         }
         temp.next = null;
         return temp;
     }
- 
-    get(index) {
-        if (index < 0 || index >= this.length) return undefined;
+
+    get ( index ) {
+        if ( index < 0 || index >= this.length ) return undefined;
         let temp = this.head;
-        for (let i = 0; i < index; i++) {
+        for ( let i = 0; i < index; i++ ) {
             temp = temp.next;
         }
         return temp;
     }
- 
-    set(index, value) {
-        let temp = this.get(index);
-        if (temp) {
+
+    set ( index, value ) {
+        let temp = this.get( index );
+        if ( temp ) {
             temp.value = value;
             return true;
         }
         return false;
     }
- 
-    insert(index, value) {
-        if (index < 0 || index > this.length) return false;
-        if (index === this.length) return this.push(value);
-        if (index === 0) return this.unshift(value);
-        
-        const newNode = new Node(value);
-        const temp = this.get(index - 1);
+
+    insert ( index, value ) {
+        if ( index < 0 || index > this.length ) return false;
+        if ( index === this.length ) return this.push( value );
+        if ( index === 0 ) return this.unshift( value );
+
+        const newNode = new Node( value );
+        const temp = this.get( index - 1 );
         newNode.next = temp.next;
         temp.next = newNode;
         this.length++;
         return true;
     }
- 
-	/// WRITE REMOVE METHOD HERE ///
-	//                            //
-	//                            //
-	//                            //
-	//                            //
-	////////////////////////////////
+
+    remove ( index ) {
+        if ( index === 0 ) {
+            return this.shift()
+        }
+        if ( index = this.length - 1 ) {
+            return this.pop()
+        }
+        const temp = this.get( index );
+        const pre = this.get( index - 1 );
+        const post = this.get( index + 1 );
+
+        pre.next = post
+        temp.next = null
+
+        this.length--;
+        return this;
+    }
+
+    /// WRITE REMOVE METHOD HERE ///
+    //                            //
+    //                            //
+    //                            //
+    //                            //
+    ////////////////////////////////
 
 }
 
 
-function test() {
-    let myLinkedList = new LinkedList(1);
-    myLinkedList.push(2);
-    myLinkedList.push(3);
-    myLinkedList.push(4);
-    myLinkedList.push(5);
+function test () {
+    let myLinkedList = new LinkedList( 1 );
+    myLinkedList.push( 2 );
+    myLinkedList.push( 3 );
+    myLinkedList.push( 4 );
+    myLinkedList.push( 5 );
 
-    console.log("LL before remove():");
+    console.log( "LL before remove():" );
     myLinkedList.printList();
 
-    console.log("\nRemoved node:");
-    console.log(myLinkedList.remove(2).value);
-    console.log("LL after remove() in middle:");
+    console.log( "\nRemoved node:" );
+    console.log( myLinkedList.remove( 2 ).value );
+    console.log( "LL after remove() in middle:" );
     myLinkedList.printList();
 
-    console.log("\nRemoved node:");
-    console.log(myLinkedList.remove(0).value);
-    console.log("LL after remove() of first node:");
+    console.log( "\nRemoved node:" );
+    console.log( myLinkedList.remove( 0 ).value );
+    console.log( "LL after remove() of first node:" );
     myLinkedList.printList();
 
-    console.log("\nRemoved node:");
-    console.log(myLinkedList.remove(2).value);
-    console.log("LL after remove() of last node:");
+    console.log( "\nRemoved node:" );
+    console.log( myLinkedList.remove( 2 ).value );
+    console.log( "LL after remove() of last node:" );
     myLinkedList.printList();
 }
 
