71ab99f44686ea996a419cdb3f5b9cfc7386d74d
junaidhkn <junaidhkniazi@gmail.com>
2023-12-02 13:35:29 +0500

    get doubly linked list

--- Notes-And-Challenges/04 - Data Structures Doubly Linked Lists/006 EXERCISE-DLL-Get.js
+++ Notes-And-Challenges/04 - Data Structures Doubly Linked Lists/006 EXERCISE-DLL-Get.js
@@ -107,7 +107,23 @@ class DoublyLinkedList {
     }
 
     /// WRITE GET METHOD HERE ///
-    get ( index ) { }
+    get ( index ) {
+        if ( index < 0 || index >= this.length ) {
+            return undefined
+        }
+        let temp = this.head
+        if ( index < this.length / 2 ) {
+            for ( let i = 0; i < index; i++ ) {
+                temp = temp.next
+            }
+        } else {
+            temp = this.tail
+            for ( let i = this.length - 1; i > index; i-- ) {
+                temp = temp.prev
+            }
+        }
+        return temp
+    }
     /////////////////////////////
 
 }
