1ff81585be8c025789a42ecdd741817699c6d544
junaidhkn <junaidhkniazi@gmail.com>
2023-12-08 23:23:22 +0500

    stack pop method

--- Notes-And-Challenges/05 - Data Structures Stacks & Queues/004 EXERCISE-Stack-Pop.js
+++ Notes-And-Challenges/05 - Data Structures Stacks & Queues/004 EXERCISE-Stack-Pop.js
@@ -1,45 +1,45 @@
 class Node {
-    constructor(value) {
+    constructor( value ) {
         this.value = value;
         this.next = null;
     }
 }
 
 class Stack {
-    constructor(value) {
-        const newNode = new Node(value);
+    constructor( value ) {
+        const newNode = new Node( value );
         this.top = newNode;
         this.length = 1;
     }
 
-    printStack() {
+    printStack () {
         let temp = this.top;
-        while (temp !== null) {
-            console.log(temp.value);
+        while ( temp !== null ) {
+            console.log( temp.value );
             temp = temp.next;
         }
     }
 
-    getTop() {
-        if (this.top === null) {
-            console.log("Top: null");
+    getTop () {
+        if ( this.top === null ) {
+            console.log( "Top: null" );
         } else {
-            console.log("Top: " + this.top.value);
+            console.log( "Top: " + this.top.value );
         }
     }
 
-    getLength() {
-        console.log("Length: " + this.length);
+    getLength () {
+        console.log( "Length: " + this.length );
     }
 
-    makeEmpty() {
+    makeEmpty () {
         this.top = null;
         this.height = 0;
     }
- 
-    push(value) {
-        const newNode = new Node(value);
-        if(this.length === 0) {
+
+    push ( value ) {
+        const newNode = new Node( value );
+        if ( this.length === 0 ) {
             this.top = newNode;
         } else {
             newNode.next = this.top;
@@ -47,41 +47,49 @@ class Stack {
         }
         this.length++;
     }
- 
-	/// WRITE POP METHOD HERE ///
-	//                         //
-	//                         //
-	//                         //
-	//                         //
-	/////////////////////////////
+
+    /// WRITE POP METHOD HERE ///
+
+    pop () {
+        if ( this.length === 0 ) {
+            return undefined
+        }
+        let temp = this.top
+        this.top = this.top.next
+        temp.next = null
+        this.length--
+        return temp
+    }
+
+    /////////////////////////////
 
 }
 
 
 
 function test () {
-    let myStack = new Stack(2);
-    myStack.push(1);
+    let myStack = new Stack( 2 );
+    myStack.push( 1 );
 
     // (2) Items - Returns 1 Node
-    if (myStack.length !== 0) {
-        console.log(myStack.pop().value);
+    if ( myStack.length !== 0 ) {
+        console.log( myStack.pop().value );
     } else {
-        console.log("undefined");
+        console.log( "undefined" );
     }
 
     // (1) Item - Returns 2 Node
-    if (myStack.length !== 0) {
-        console.log(myStack.pop().value);
+    if ( myStack.length !== 0 ) {
+        console.log( myStack.pop().value );
     } else {
-        console.log("undefined");
+        console.log( "undefined" );
     }
 
     // (0) Items - Returns undefined
-    if (myStack.length !== 0) {
-        console.log(myStack.pop().value);
+    if ( myStack.length !== 0 ) {
+        console.log( myStack.pop().value );
     } else {
-        console.log("undefined");
+        console.log( "undefined" );
     }
 }
 
