f75313d58124ddf0e9adf4123acfe10ba0e34e4c
junaidhkn <junaidhkniazi@gmail.com>
2023-12-09 21:11:08 +0500

    Enqueue

--- Notes-And-Challenges/05 - Data Structures Stacks & Queues/007 EXERCISE-Queue-Enqueue.js
+++ Notes-And-Challenges/05 - Data Structures Stacks & Queues/007 EXERCISE-Queue-Enqueue.js
@@ -49,7 +49,20 @@ class Queue {
 
     // WRITE ENQUEUE METHOD HERE //
 
-
+    enqueue ( value ) {
+        const newNode = new Node( value );
+        if ( this.length === 0 ) {
+            this.first = newNode
+            this.last = newNode
+            this.length = 1
+        }
+        else {
+            this.last.next = newNode
+            this.last = newNode
+            this.length++
+        }
+        return this
+    }
 
     ///////////////////////////////
 
