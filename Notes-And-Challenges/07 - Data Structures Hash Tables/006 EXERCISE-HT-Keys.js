423a54a38089b58030385a8d83b6c31aebd7d508
junaidhkn <junaidhkniazi@gmail.com>
2024-02-17 02:45:13 +0500

    push all keys hash table

--- Notes-And-Challenges/07 - Data Structures Hash Tables/006 EXERCISE-HT-Keys.js
+++ Notes-And-Challenges/07 - Data Structures Hash Tables/006 EXERCISE-HT-Keys.js
@@ -1,35 +1,35 @@
 class HashTable {
-    constructor(size = 7) {
-        this.dataMap = new Array(size);
+    constructor( size = 7 ) {
+        this.dataMap = new Array( size );
     }
-   
-    _hash(key) {
+
+    _hash ( key ) {
         let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+        for ( let i = 0; i < key.length; i++ ) {
+            hash = ( hash + key.charCodeAt( i ) * 23 ) % this.dataMap.length;
         }
         return hash;
     }
 
-    printTable() {
-        for (let i = 0; i < this.dataMap.length; i++) {
-            console.log(i, ": ", this.dataMap[i]);
+    printTable () {
+        for ( let i = 0; i < this.dataMap.length; i++ ) {
+            console.log( i, ": ", this.dataMap[i] );
         }
     }
 
-    set(key,value) {
-        let index = this._hash(key);
-        if(!this.dataMap[index]) this.dataMap[index] = [];
-        
-        this.dataMap[index].push([key, value]);
+    set ( key, value ) {
+        let index = this._hash( key );
+        if ( !this.dataMap[index] ) this.dataMap[index] = [];
+
+        this.dataMap[index].push( [key, value] );
         return this;
     }
 
-    get(key) {
-        let index = this._hash(key);
-        if(this.dataMap[index]) {
-            for(let i = 0; i < this.dataMap[index].length; i++) {
-                if(this.dataMap[index][i][0] === key) {
+    get ( key ) {
+        let index = this._hash( key );
+        if ( this.dataMap[index] ) {
+            for ( let i = 0; i < this.dataMap[index].length; i++ ) {
+                if ( this.dataMap[index][i][0] === key ) {
                     return this.dataMap[index][i][1];
                 }
             }
@@ -37,26 +37,34 @@ class HashTable {
         return undefined;
     }
 
-	///  WRITE KEYS METHOD HERE  ///
-	//                            //
-	//                            //
-	//                            //
-	//                            //
-	////////////////////////////////
+    ///  WRITE KEYS METHOD HERE  ///
+
+    keys () {
+        let allKeys = []
+        for ( let i = 0; i < this.dataMap.length; i++ ) {
+            if ( this.dataMap[i] ) {
+                for ( let j = 0; j < this.dataMap[i].length; j++ ) {
+                    allKeys.push( this.dataMap[i][j][0] )
+                }
+            }
+        }
+    }
+
+    ////////////////////////////////
 
 }
 
 
 
-function test() {
+function test () {
 
     let myHashTable = new HashTable();
 
-    myHashTable.set("paint", 20);
-    myHashTable.set("bolts", 40);
-    myHashTable.set("nails", 100);
-    myHashTable.set("tile", 50);
-    myHashTable.set("lumber", 80);
+    myHashTable.set( "paint", 20 );
+    myHashTable.set( "bolts", 40 );
+    myHashTable.set( "nails", 100 );
+    myHashTable.set( "tile", 50 );
+    myHashTable.set( "lumber", 80 );
 
     console.log( myHashTable.keys() );
 }
@@ -70,4 +78,4 @@ test();
     ----------------
     [ 'paint', 'bolts', 'nails', 'tile', 'lumber' ]
 
-*/   
+*/
