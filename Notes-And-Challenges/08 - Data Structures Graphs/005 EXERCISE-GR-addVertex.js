2068ac575bb1576afe6cc4ca0dcf605aebf258c6
junaidhkn <junaidhkniazi@gmail.com>
2024-02-17 23:47:12 +0500

    Big O of Graph

--- Notes-And-Challenges/08 - Data Structures Graphs/005 EXERCISE-GR-addVertex.js
+++ Notes-And-Challenges/08 - Data Structures Graphs/005 EXERCISE-GR-addVertex.js
@@ -1,32 +1,49 @@
+/*
+
+> Single item in graph is refered to as a Vertex(also often called as Node).
+> And the connection between these Vertex is called Edge(also often called as connection)
+> These is no limitations to how many vertices a particular vertex can connect to.
+> There are couple of ways we can store a graph 
+        - Adjacency Matrix
+        - Adjacency List
+> Space Complexity of 
+        - Adjacency Matrix => O(|V|^2), where V is vertices
+        - Adjacency List => O(|V| + |E|), where V is vertices and edges
+> Time Complexity of 
+        - Adjacency Matrix => O(|V|^2)
+        - Adjacency List   => O(1)
+
+*/
+
 class Graph {
     constructor() {
         this.adjacencyList = {};
     }
 
-    printGraph() {
-        if (Object.keys(this.adjacencyList).length !== 0) {
-            console.log("{");
-            for (const [key, value] of Object.entries(this.adjacencyList)) {
-                console.log(" ", `${key}: ${value}`);
+    printGraph () {
+        if ( Object.keys( this.adjacencyList ).length !== 0 ) {
+            console.log( "{" );
+            for ( const [key, value] of Object.entries( this.adjacencyList ) ) {
+                console.log( " ", `${key}: ${value}` );
             }
-            console.log("}");
+            console.log( "}" );
         } else {
-            console.log("{}");
+            console.log( "{}" );
         }
     }
 
-	/// WRITE ADDVERTEX METHOD HERE ///
-	//                               //
-	//                               //
-	//                               //
-	//                               //
-	///////////////////////////////////
+    /// WRITE ADDVERTEX METHOD HERE ///
+    //                               //
+    //                               //
+    //                               //
+    //                               //
+    ///////////////////////////////////
 
 }
 
 
 
-function test() {
+function test () {
     let myGraph = new Graph();
     myGraph.printGraph();
 }
@@ -40,4 +57,4 @@ test();
     ----------------
     {}
 
-*/   
+*/
