032526b06a6531689c0e8fa9b8a44d34300d06da
junaidhkn <junaidhkniazi@gmail.com>
2024-07-02 23:48:44 +0500

    nth sequence

--- Computer Science/1-Intro to DSA/specs/recursion/factorials.test.js
+++ Computer Science/1-Intro to DSA/specs/recursion/factorials.test.js
@@ -71,7 +71,13 @@ Both implementations efficiently compute the Fibonacci sequence, but the iterati
 
  */
 
-
+function fibonacci ( n ) {
+  const sequence = [0, 1];
+  for ( let i = 2; i < n + 1; i++ ) {
+    sequence.push( sequence[i - 2] + sequence[i - 1] );
+  }
+  return sequence[n];
+}
 
 
 function factorial ( n ) { }
