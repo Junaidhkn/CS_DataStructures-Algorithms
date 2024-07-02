00d404deb7933ed0c30b009a2391c825c29746ba
junaidhkn <junaidhkniazi@gmail.com>
2024-07-02 18:09:41 +0500

    Insertion sort

--- Computer Science/1-Intro to DSA/specs/insertion-sort/insertion-sort.test.js
+++ Computer Science/1-Intro to DSA/specs/insertion-sort/insertion-sort.test.js
@@ -14,7 +14,16 @@
 */
 
 function insertionSort ( nums ) {
-
+  for ( let i = 1; i < nums.length; i++ ) {
+    let current = nums[i];
+    let j = i - 1;
+    while ( j >= 0 && nums[j] > current ) {
+      nums[j + 1] = nums[j];
+      j--;
+    }
+    nums[j + 1] = current;
+  }
+  return nums;
 }
 
 let array = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
