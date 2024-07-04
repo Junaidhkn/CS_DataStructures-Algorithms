33c0435d0ea6b2dfeea876b210f7957e5464e35c
junaidhkn <junaidhkniazi@gmail.com>
2024-07-04 22:01:07 +0500

    quick sort test

--- Computer Science/1-Intro to DSA/specs/quick-sort/quick-sort.test.js
+++ Computer Science/1-Intro to DSA/specs/quick-sort/quick-sort.test.js
@@ -31,13 +31,13 @@ function quickSort ( nums ) {
   return [...quickSort( left ), pivot, ...quickSort( right )];
 }
 
-console.log( quickSort( [10, 8, 2, 1, 6, 3, 9, 4, 7, 5] ) )
+
 
 // unit tests
 // do not modify the below code
-// test.skip( "quickSort", function () {
-//   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
-//   const answer = quickSort( input );
+test( "quickSort", function () {
+  const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
+  const answer = quickSort( input );
 
-//   expect( answer ).toEqual( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] );
-// } );
+  expect( answer ).toEqual( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] );
+} );
