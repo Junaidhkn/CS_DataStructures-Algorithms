5fddf8c946253248cdfd719e43827ed6fdf997e4
junaidhkn <junaidhkniazi@gmail.com>
2024-09-03 23:17:50 +0500

    array list

--- Computer Science/1-Intro to DSA/specs/arrays/arraylist.test.js
+++ Computer Science/1-Intro to DSA/specs/arrays/arraylist.test.js
@@ -18,7 +18,40 @@
 */
 
 class ArrayList {
-  // code goes here
+
+  constructor() {
+    this.length = 0
+    this.data = {}
+  }
+
+  pop () {
+    this.length--
+    const item = this.data[this.length]
+    delete this.data[this.length - 1]
+    return item
+  }
+  push ( value ) {
+    this.data[this.length] = value
+    this.length++
+  }
+  get ( index ) {
+    return this.data[index]
+  }
+  delete ( index ) {
+    const ans = this.data[index];
+    this._collapseTo( index );
+    return ans;
+  }
+  _collapseTo ( index ) {
+    for ( let i = index; i < this.length; i++ ) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+  serialize () {
+    return this.data;
+  }
 }
 
 // unit tests
