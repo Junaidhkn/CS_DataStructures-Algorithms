879ebf42564e9842e480987124164c94d9d8b08d
junaidhkn <junaidhkniazi@gmail.com>
2024-09-06 23:21:01 +0500

    Continue statement

--- Computer Science/2-Algorithms/src/day1/BTBFS.ts
+++ Computer Science/2-Algorithms/src/day1/BTBFS.ts
@@ -3,6 +3,7 @@ export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
     while (queue.length) {
         const curr = queue.shift() as BinaryNode<number> | null | undefined;
+        //    In a BFS traversal, when nodes are added to the queue, their left and right children are also added regardless of whether they are null or not.This can result in null values being added to the queue.Without the continue statement, if curr is null, attempting to access curr.value would cause an error since null does not have a value property.continue helps to bypass these potential null values and keeps the loop running smoothly, only processing actual nodes.
         if (!curr) continue;
 
         if (curr.value === needle) return true;
