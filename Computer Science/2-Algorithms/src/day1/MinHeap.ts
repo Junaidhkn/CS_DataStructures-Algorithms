96124561f1409c498e9344fd9aabe6f9c9eb52aa
junaidhkn <junaidhkniazi@gmail.com>
2024-09-09 02:12:56 +0500

    MIn heap implementation

--- Computer Science/2-Algorithms/src/day1/MinHeap.ts
+++ Computer Science/2-Algorithms/src/day1/MinHeap.ts
@@ -30,17 +30,45 @@ export default class MinHeap {
         this.data = [];
     }
 
-    insert(value: number): void {}
-    delete(): number {}
+    insert(value: number): void {
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
+    }
+    // pop function
+    delete(): number {
+        if (this.length === 0) return -1;
+        const out = this.data[0];
+        this.length--;
+        if (this.length === 0) {
+            this.data = [];
+            return out;
+        }
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0);
+        return out;
+    }
 
     private heapifyDown(idx: number): void {
         const leftIndex = this.leftChild(idx);
-        const rightChild = this.rightChild(idx);
+        const rightIndex = this.rightChild(idx);
 
         if (idx >= this.length || leftIndex >= this.length) {
             return;
         }
-        const leftValue = this.data[]
+        const leftValue = this.data[leftIndex];
+        const rightValue = this.data[rightIndex];
+        const value = this.data[idx];
+
+        if (leftValue > rightValue && value > rightValue) {
+            this.data[idx] = rightValue;
+            this.data[rightIndex] = value;
+            this.heapifyDown(rightIndex);
+        } else if (rightValue > leftValue && value > leftValue) {
+            this.data[idx] = leftValue;
+            this.data[leftIndex] = value;
+            this.heapifyDown(leftIndex);
+        }
     }
 
     private heapifyUp(idx: number): void {
