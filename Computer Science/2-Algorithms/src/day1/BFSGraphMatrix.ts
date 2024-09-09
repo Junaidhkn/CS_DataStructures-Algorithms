a0ec2031e270389f64bf8bf73c5c3448709e34eb
junaidhkn <junaidhkniazi@gmail.com>
2024-09-10 00:21:52 +0500

    BFS Graph Matrix

--- Computer Science/2-Algorithms/src/day1/BFSGraphMatrix.ts
+++ Computer Science/2-Algorithms/src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,13 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
 
+    seen[source] = true;
+    const q: number[] = [source];
+
+    do {} while (q.length);
 }
