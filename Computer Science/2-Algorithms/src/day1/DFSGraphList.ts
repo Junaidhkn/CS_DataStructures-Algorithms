a777edeeb8f435182a9e70eb8a615ad1388325ab
junaidhkn <junaidhkniazi@gmail.com>
2024-09-11 04:16:36 +0500

    walk recursive

--- Computer Science/2-Algorithms/src/day1/DFSGraphList.ts
+++ Computer Science/2-Algorithms/src/day1/DFSGraphList.ts
@@ -1,3 +1,32 @@
+function walk(
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    if (curr === needle) {
+        return true;
+    }
+
+    if (seen[curr]) {
+        return false;
+    }
+
+    seen[curr] = true;
+
+    // recurse
+
+    // pre
+    path.push(curr);
+    // recurse
+    const list = graph[curr];
+
+    // post
+    path.pop();
+    return false;
+}
+
 export default function dfs(
     graph: WeightedAdjacencyList,
     source: number,
