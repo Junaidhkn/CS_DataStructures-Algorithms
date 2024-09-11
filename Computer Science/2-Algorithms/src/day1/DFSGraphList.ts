7a89fd3accba0331cc5a1045af893bf359446033
junaidhkn <junaidhkniazi@gmail.com>
2024-09-12 01:44:44 +0500

    Depth first search adjacency list

--- Computer Science/2-Algorithms/src/day1/DFSGraphList.ts
+++ Computer Science/2-Algorithms/src/day1/DFSGraphList.ts
@@ -5,24 +5,26 @@ function walk(
     seen: boolean[],
     path: number[],
 ): boolean {
-    if (curr === needle) {
-        return true;
-    }
-
     if (seen[curr]) {
         return false;
     }
 
     seen[curr] = true;
 
-    // recurse
-
-    // pre
     path.push(curr);
-    // recurse
-    const list = graph[curr];
 
-    // post
+    if (curr === needle) {
+        return true;
+    }
+
+    const list = graph[curr];
+    for (let i = 0; i < list.length; ++i) {
+        const edge = list[i];
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
     path.pop();
     return false;
 }
@@ -31,4 +33,13 @@ export default function dfs(
     graph: WeightedAdjacencyList,
     source: number,
     needle: number,
-): number[] | null {}
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = [];
+
+    walk(graph, source, needle, seen, path);
+
+    if (path.length === 0) return null;
+
+    return path;
+}
