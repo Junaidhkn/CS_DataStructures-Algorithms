f6fb440a0366cfc76e0ab19cecb38dd6d628b3de
junaidhkn <junaidhkniazi@gmail.com>
2026-03-20 00:49:33 +0500

    sliding window problem

--- python/algorithms/patterns/3-sliding window/sliding-window.py
+++ python/algorithms/patterns/3-sliding window/sliding-window.py
@@ -95,8 +95,30 @@ Explanation: The subarrays of nums with length 3 are:
 We return 0 because no subarrays meet the conditions.
  
 
-Constraints:
+Constraints: 
 
 1 <= k <= nums.length <= 105
 1 <= nums[i] <= 105
 """
+
+
+def maximumSubarraySum(nums, k):
+    seen = set()
+    left = 0
+    current_sum = 0
+    max_sum = 0
+
+    for right in range(len(nums)):
+        while nums[right] in seen:
+            seen.remove(nums[left])
+            current_sum -= nums[left]
+            left += 1
+        seen.add(nums[right])
+        current_sum += nums[right]
+        if right - left + 1 == k:
+            max_sum = max(max_sum, current_sum)
+            seen.remove(nums[left])
+            current_sum -= nums[left]
+            left += 1
+
+    return max_sum
