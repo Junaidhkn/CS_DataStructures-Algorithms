620a325706b2838ed75e0d0a0f703f7019fab072
junaidhkn <junaidhkniazi@gmail.com>
2026-07-01 06:38:42 +0500

    more problems

--- python/algorithms/patterns/1-two pointers/two-pointers-problems.py
+++ python/algorithms/patterns/1-two pointers/two-pointers-problems.py
@@ -369,24 +369,114 @@ def sortedSquaresOptimal(nums: list[int]) -> list[int]:
     n = len(nums)
     result = [0] * n
     left, right = 0, n - 1
-    pos = n - 1  # fill result from the back
-
+    pos = n - 1
     while left <= right:
         left_sq = nums[left] ** 2
         right_sq = nums[right] ** 2
-
         if left_sq > right_sq:
             result[pos] = left_sq
             left += 1
         else:
             result[pos] = right_sq
             right -= 1
-
         pos -= 1
-
     return result
 
 
 result = sortedSquaresOptimal([-4, -1, 0, 3, 10])
 
 print("Printing result from question 5:\n", result)
+
+
+#! Question 6: Triplet Sum to Zero (medium)
+
+"""
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+"""
+
+
+#! Question 7: 	Triplet Sum Close to Target (medium)
+
+"""
+
+Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ 
+
+Constraints:
+
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+
+"""
+
+
+#! Question 8: 	Triplets with Smaller Sum (medium)
+
+
+"""
+
+Given an array arr[] of distinct integers and a value sum, find the count of triplets (i, j, k), having (i<j<k) with the sum of (arr[i] + arr[j] + arr[k]) smaller than the given value sum.
+
+Examples :
+
+Input: sum = 2, arr[] = [-2, 0, 1, 3]
+Output:  2
+Explanation: Triplets with sum less than 2 are (-2, 0, 1) and (-2, 0, 3). 
+Input: sum = 12, arr[] = [5, 1, 3, 4, 7]
+Output: 4
+Explanation: Triplets with sum less than 12 are (1, 3, 4), (5, 1, 3), (1, 3, 7) and (5, 1, 4).
+Constraints:
+1 ≤ sum ≤ 105
+3 ≤ arr.size() ≤ 103
+-103 ≤ arr[i] ≤ 103
+
+"""
