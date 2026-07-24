51b8517e3ea56b1436c674e2aa14bf97c28b4700
junaidhkn <junaidhkniazi@gmail.com>
2026-07-25 04:04:21 +0500

    min subarray len

--- python/algorithms/patterns/3-sliding window/sliding-window.py
+++ python/algorithms/patterns/3-sliding window/sliding-window.py
@@ -137,6 +137,87 @@ print("Optimized solution for question 1:\n", result)
 
 # Time complexity : O(n) Space Complexity : O(1)
 
+
+## ! Question 2: Minimum size subarray sum
+
+
+"""
+Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+"""
+
+# Brute Force solution:
+
+
+def minSubArrayLenBF(target, nums):
+    n = len(nums)
+    minimum = float("inf")
+
+    for i in range(n):
+
+        current_sum = 0
+
+        for j in range(i, n):
+
+            current_sum += nums[j]
+
+            if current_sum >= target:
+                minimum = min(minimum, j - i + 1)
+                break
+
+    return 0 if minimum == float("inf") else minimum
+
+
+# Sliding window : O(n) solution
+def minSubArrayLen(target, nums):
+    left = 0
+    curr_sum = 0
+    min_len = float("inf")
+
+    for right in range(len(nums)):
+        curr_sum += nums[right]
+
+        # Shrink window while valid
+        while curr_sum >= target:
+            min_len = min(min_len, right - left + 1)
+            curr_sum -= nums[left]
+            left += 1
+
+    return 0 if min_len == float("inf") else min_len
+
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+
+
+# print("printing solution for minsubarraylen", minSubArrayLen(10, [2, 3, 1, 2, 4, 3]))
+
+
 ## ! Question 1: Maximum sum of distinct subarrays with length K
 """
 You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
